6b0ffd971ca291e805f927592e670cd33e38ecbe
chensungyu <kobe29856907@gmail.com>
2022-08-17 09:55:49 +0800

    LeetCode medium problem is not OK

---
+++ 300_Longest_Increasing_Subsequence.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define max(a,b) (a > b ? a : b)
+
+/* brute force*/
+int method1(int* nums, int numsSize) {
+ 
+    if (numsSize <= 1)
+        return numsSize;
+    
+    int maxRes = 1, dp[numsSize]; // dp[i] means the LIS with end of value nums[i]
+    dp[0] = 1;
+    for (int i = 1; i < numsSize; i++)
+    {
+        dp[i] = 1;
+        for (int j = 0; j < i; j++)
+        {
+            if (nums[i] > nums[j])
+            {
+                dp[i] = max(dp[i], dp[j]+1);
+            }
+        }
+        if (maxRes < dp[i])
+            maxRes = dp[i];
+    }
+    
+    return maxRes;
+}
+
+int lengthOfLIS(int* nums, int numsSize)
+{
+
+}
